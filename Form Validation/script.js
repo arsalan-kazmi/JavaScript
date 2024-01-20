c66bee5d37f8d170dd963c62e93429efc7ed1d98
arsalankazmi7 <arsalankazmi7@gmail.com>
2024-01-20 20:34:06 +0530

    Initial commit JS

---
+++ Form Validation/script.js
@@ -0,0 +1,81 @@
+let nameError = document.getElementById("name-error");
+let phoneError = document.getElementById("phone-error");
+let emailError = document.getElementById("email-error");
+let messageError = document.getElementById("message-error");
+let submitError = document.getElementById("submit-error");
+
+function validateName() {
+  let name = document.getElementById("contact-name").value;
+
+  if (name.length == 0) {
+    nameError.innerHTML = "Name is required";
+    return false;
+  }
+  if (!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)) {
+    nameError.innerHTML = "Write full name";
+    return false;
+  }
+  nameError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+  return true;
+}
+
+function validatePhone() {
+  let phone = document.getElementById("contact-phone").value;
+
+  if (phone.length == 0) {
+    phoneError.innerHTML = "Phone number is required";
+    return false;
+  }
+  if (!phone.match(/^[0-9]{10}$/)) {
+    phoneError.innerHTML = "Invalid phone number";
+    return false;
+  }
+  // if(phone.length !== 10){
+  //   phoneError.innerHTML = '10 digits';
+  //   return false;
+  // }
+
+  phoneError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+  return true;
+}
+
+function validateEmail() {
+  let email = document.getElementById("contact-email").value;
+
+  if (email.length == 0) {
+    emailError.innerHTML = "Email is required";
+    return false;
+  }
+
+  if (!email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) {
+    emailError.innerHTML = "Invalid email";
+    return false;
+  }
+
+  emailError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+  return true;
+}
+
+function validateMessage() {
+  let message = document.getElementById("contact-message").value;
+
+  let required = 20;
+  let left = required - message.length;
+
+  if (left > 0) {
+    messageError.innerHTML = `${left} more characters required`;
+    return false;
+  }
+  messageError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+  return true;
+}
+
+function validateForm(){
+  if(!validateName() || !validatePhone() || !validateEmail() || !validateMessage()){
+    submitError.style.display = 'block';
+    submitError.innerHTML = 'Please fix the errors to submit';
+    setTimeout(function(){submitError.style.display = 'none';}, 3000);
+    return false;
+  }
+  
+}
